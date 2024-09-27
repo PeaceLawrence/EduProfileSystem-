445e0211018f0934876d8dc723737c0c15f75d5f
Peace Enweriku <peacelawrence555@gmail.com>
2024-09-27 04:31:05 +0000

    Completed Code

--- Main.java
+++ Main.java
@@ -3,12 +3,20 @@ class Main {
 	
     public static void main(String[] args) {
 
+		Student student1 = new Student("Peace", 19, 6, 180, 42, 3.5, "Computer Science");
+		System.out.println(student1.toString());
+		System.out.println(student1.status());
+		System.out.println(student1.getMajor());
+		System.out.println(student1.getGPA() + " GPA");
+		System.out.println(student1.getEarnedCreds());
+
+		System.out.println();
 
 		Professor professor1 = new Professor("Archie", 42, 6, 175, 
 		30, 2.8, "Computer Science");
 		System.out.println(professor1.toString());
 		System.out.println(professor1.getTeachingLoad());
-		System.out.println(professor1.getStudentEval());
+		System.out.println(professor1.getStudentEval() + " Rating");
 		System.out.println(professor1.getDepartment());
 		System.out.println(professor1.getRating());
   }
